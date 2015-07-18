9ecd3692a237083c53a3aaffa2c925ecc6360e1c
init <erin@igyeonglyunui-MacBook-Pro.local>
2015-07-19 08:33:56 +0900

    init

---
+++ Table.js
@@ -0,0 +1,17 @@
+function Table (jsonData) {
+	this.jsonData = jsonData;
+}
+
+Table.prototype.getTable = function(option) {
+	this.option = option;	
+};
+
+Table.prototype.getUl = function(option) {
+	this.option = option;	
+	
+};
+
+Table.prototype.getCanvas = function(option) {
+	this.option = option;	
+	
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+var jsonData = [
+	{
+		id: erin314,
+		pw: 1234,
+		name: erin
+	},{
+		id: erin314,
+		pw: 1234,
+		name: erin
+	},{
+		id: erin314,
+		pw: 1234,
+		name: erin
+	},{
+		id: erin314,
+		pw: 1234,
+		name: erin
+	},{
+		id: erin314,
+		pw: 1234,
+		name: erin
+	},
+];
+
+var source = new Table(jsonData);
+
+source.getTable();
+source.getUl();
+source.getUl({
+	setClassName,
+	setPlusEvent,
+	setMinusEvent
+});
+source.getCanvas;
